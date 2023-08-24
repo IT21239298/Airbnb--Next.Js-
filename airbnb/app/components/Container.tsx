9363314bfd351f333da1b38868bdfcf239084fd0
IT21239298 <it21239298@my.sliit.lk>
2023-08-24 23:05:22 +0530

    container

---
+++ airbnb/app/components/Container.tsx
@@ -0,0 +1,8 @@
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div>{children}</div>;
+};
+export default Container;
--- airbnb/app/components/navbar/Navbar.tsx
+++ airbnb/app/components/navbar/Navbar.tsx
@@ -1,4 +1,17 @@
+import Container from "../Container";
+
 const Navbar = () => {
-  return <div className="fixed w-full bg-white z-10 shadow-sm">Navbar</div>;
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+    py-4
+    border-b-[1px]"
+      >
+        <Container>anuthing</Container>
+      </div>
+      Navbar
+    </div>
+  );
 };
 export default Navbar;
