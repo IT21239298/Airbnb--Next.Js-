9cb03e39a8a360b6392403ca4671b949893877da
IT21239298 <it21239298@my.sliit.lk>
2023-08-25 23:01:42 +0530

    change position and create menu

--- airbnb/app/components/navbar/Navbar.tsx
+++ airbnb/app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
@@ -10,6 +11,7 @@ const Navbar = () => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
