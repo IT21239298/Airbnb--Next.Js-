61ba122ff5d95e3148c7415d89bce712023c6e49
IT21239298 <it21239298@my.sliit.lk>
2023-08-25 22:09:59 +0530

    implement search

--- airbnb/app/components/navbar/Navbar.tsx
+++ airbnb/app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,7 @@ const Navbar = () => {
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
+            <Search />
           </div>
         </Container>
       </div>
