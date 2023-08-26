ecb50b922527b0fd249c547ccbeec3e746f0bf8e
IT21239298 <it21239298@my.sliit.lk>
2023-08-26 14:54:46 +0530

    add translate

--- airbnb/app/components/modals/Modal.tsx
+++ airbnb/app/components/modals/Modal.tsx
@@ -61,7 +61,16 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       <div className=" justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto"></div>
+        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+          {/* content */}
+          <div
+            className={`translate duration-300 h-full
+            ${showModel ? "translate-y-0" : "translate-y-full"}   
+            ${showModel ? "opacity-100" : "opacity-0"}
+             
+            `}
+          ></div>
+        </div>
       </div>
     </>
   );
