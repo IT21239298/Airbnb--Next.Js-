3645a91a91d7c79b83284d32b52b3947e9db3260
IT21239298 <it21239298@my.sliit.lk>
2023-08-27 23:33:09 +0530

    button

--- airbnb/app/components/modals/Modal.tsx
+++ airbnb/app/components/modals/Modal.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -83,6 +84,12 @@ const Modal: React.FC<ModalProps> = ({
               </div>
               {/* Body*/}
               <div className="relative p-6 flex-auto">{body}</div>
+            </div>
+            {/* footer*/}
+            <div className="flex flex-col gap-2 p-6">
+              <div className="flex flex-row items-center gap-4 w-full">
+                <Button />
+              </div>
             </div>
           </div>
         </div>
