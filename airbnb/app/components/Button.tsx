ffbbc28fa6dcfceb56a0e57ad39b66a0900623af
IT21239298 <it21239298@my.sliit.lk>
2023-08-30 22:19:24 +0530

    styles

--- airbnb/app/components/Button.tsx
+++ airbnb/app/components/Button.tsx
@@ -30,7 +30,10 @@ const Button: React.FC<ButtonProps> = ({
   ${outline ? "bg-white " : "bg-rose-500"}
   ${outline ? "border-black " : "border-rose-500"}
   ${outline ? "text-black " : "text-white"}
-  
+  ${small ? "py-1" : "py-3"}
+  ${small ? "text-sm" : "text-md"}
+  ${small ? "font-light" : "font-semibold"}
+  ${small ? "border-[1px]" : "border-2"}
   
   `}
     >
