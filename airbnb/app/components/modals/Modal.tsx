ab38f6e810f9c72a17b398dfa7b6c90c35c9a572
IT21239298 <it21239298@my.sliit.lk>
2023-09-07 23:52:08 +0530

    update

--- airbnb/app/components/modals/Modal.tsx
+++ airbnb/app/components/modals/Modal.tsx
@@ -85,12 +85,15 @@ const Modal: React.FC<ModalProps> = ({
               {/* footer*/}
               <div className="flex flex-col gap-2 p-6">
                 <div className="flex flex-row items-center gap-4 w-full">
-                  <Button
-                    outline
-                    disabled={disabled}
-                    label={secondaryActionLabel}
-                    onClick={handleSecondaryAction}
-                  />
+                  {secondaryAction && secondaryActionLabel && (
+                    <Button
+                      outline
+                      disabled={disabled}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                    />
+                  )}
+
                   <Button
                     disabled={disabled}
                     label={actionLabel}
