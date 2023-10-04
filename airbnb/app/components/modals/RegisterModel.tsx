8ab66d21b890db4c92567aaf97e172e3d21eb4eb
IT21239298 <it21239298@my.sliit.lk>
2023-10-04 17:59:30 +0530

    register model

--- airbnb/app/components/modals/RegisterModel.tsx
+++ airbnb/app/components/modals/RegisterModel.tsx
@@ -7,6 +7,8 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import { error } from "console";
+import Modal from "./Modal";
 
 const RegisterModel = () => {
   const RegisterModel = useRegisterModal();
@@ -24,7 +26,32 @@ const RegisterModel = () => {
     },
   });
 
-  return <div>RegisterModel</div>;
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setisLoading(true);
+
+    axios
+      .post("api/register", data)
+      .then(() => {
+        RegisterModel.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setisLoading(false);
+      });
+  };
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={RegisterModel.isOpen}
+      title="Register"
+      actionLabel={"Continue"}
+      onClose={RegisterModel.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
 };
 
 export default RegisterModel;
