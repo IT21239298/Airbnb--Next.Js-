498922ef47e9fce29c431e36bbdfa1aef9384821
IT21239298 <it21239298@my.sliit.lk>
2023-10-04 23:31:43 +0530

    input interface

--- airbnb/app/components/inputs/Input.tsx
+++ airbnb/app/components/inputs/Input.tsx
@@ -1,6 +1,18 @@
 "use client";
 
 import React from "react";
+import { FieldErrors, FieldValue, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValue>;
+  errors: FieldErrors;
+}
 
 const Input = () => {
   return <div>Input</div>;
