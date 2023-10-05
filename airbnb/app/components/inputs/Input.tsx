e000633022956390c423f459dff78d9719c9e53b
IT21239298 <it21239298@my.sliit.lk>
2023-10-06 02:10:47 +0530

    input field

--- airbnb/app/components/inputs/Input.tsx
+++ airbnb/app/components/inputs/Input.tsx
@@ -33,6 +33,30 @@ const Input: React.FC<InputProps> = ({
           className="text-neutral-700 absolute top-5 left-2"
         />
       )}
+      <input
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=" "
+        type={type}
+        className={`
+          peer
+          w-full
+          p-4
+          pt-6
+          font-light
+          bg-white
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+          ${formatPrice ? "pl-9" : "pl-4"}
+          ${errors[id] ? "border-rose-500" : " border-neutral-300"}
+          ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
+        `}
+      />
     </div>
   );
 };
--- airbnb/app/components/modals/RegisterModel.tsx
+++ airbnb/app/components/modals/RegisterModel.tsx
@@ -47,7 +47,15 @@ const RegisterModel = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
-      <Input />
+
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </div>
   );
 
