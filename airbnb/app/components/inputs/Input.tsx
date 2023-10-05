11fe58f04e2ba6c4595f174afa95cd1d3b186c1c
IT21239298 <it21239298@my.sliit.lk>
2023-10-06 02:25:14 +0530

    create label

--- airbnb/app/components/inputs/Input.tsx
+++ airbnb/app/components/inputs/Input.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { error } from "console";
 import React from "react";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
@@ -57,6 +58,27 @@ const Input: React.FC<InputProps> = ({
           ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
         `}
       />
+      <label
+        className={`
+      absolute
+      text-md
+      duration-150
+      transform
+      -translate-y-3
+      top-5
+      z-10
+      origin-[0]
+      ${formatPrice ? "left-9" : "left-4"}
+      peer-placeholder-shown:scale-100
+      peer-placeholder-shown:translate-y-0
+      peer-focus:scale-75
+      peer-focus:-translate-y-4
+      ${errors[id] ? "text-rose-500" : "text-zinc-400"} 
+
+      `}
+      >
+        {label}
+      </label>
     </div>
   );
 };
